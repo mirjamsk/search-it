c7a551223198a3f6a4ba0c1c71b38835f92c1582
mirjamsk <mirjamskarica@gmail.com>
2016-06-05 00:31:48 +0200

    Add search-it v0.1.0

---
+++ main.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    var headers = $('div.collapsible-header');
+
+    $('.collapsible').collapsible({
+        accordion: false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+    $('#searchable-1').searchIt({
+        useMaterializeCollapsible: true,
+        headerIdentifier: '.collapsible-header',
+        itemSelector: '.collapsible-body a',
+        searchTemplate: '<div class="input-field">' +
+            '<input id="navbar-search" type="text">' +
+            '<label for="navbar-search">Pretraži</label>' +
+            '</div>'
+    });
+
+    $('#searchable-2').searchIt({
+        itemSelector: 'a',
+        inputLabelValue: 'Looking for something?',
+    });
+
+    $('#searchable-3').searchIt({
+        itemSelector: 'a',
+        inputLabelValue: 'Looking for something?',
+        $searchInput: $('#input-container-3').find('input'),
+    });
+    $('#searchable-4').searchIt({
+        itemSelector: 'p',
+    });
+});
